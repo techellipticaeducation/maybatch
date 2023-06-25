ba1459137c39878d6a470d76eb9a6b975b56c5d0
Teche <techellipticaeducation@gmail.com>
2023-06-25 11:27:57 +0530

    updates

--- src/humanchat/Chatter.java
+++ src/humanchat/Chatter.java
@@ -4,29 +4,35 @@ public class Chatter {
 	public static void main(String[] args) {
 		// Nirav , Ayesha
 		
+		Human.country = "India";
+		
+		
 		Human h1 = new Human();
 		h1.name = "Nirav";
-		h1.country = "India";
 		h1.age = 25; 
-				
+			
+		
+		
 		Human h2 = new Human();
 		h2.name = "Ayesha";
-		h2.country = "US";
 		h2.age = 24;
 		
+		Human h5 = h2;
+		
+		
 		h1.speak("Hello");
-		h2.speak("Hey! How r u?");
+		
+		h5.speak("Hey! How r u?");
 		
 		Human h3 = new Human();
 		h3.name = "Sufiyan";
-		h3.country = "Aus";
 		h3.age = 26;
 		
 		h3.speak("hey Guys! how r doing today ? ");
 		h1.happyBirthday();
 		h1.happyBirthday();
 		h3.happyBirthday();
-		h2.happyBirthday();
+		h5.happyBirthday();
 		
 	}
 }
--- src/humanchat/Human.java
+++ src/humanchat/Human.java
@@ -3,8 +3,19 @@ package humanchat;
 public class Human {
 
 	String name;
-	String country;
+	static String country;
 	int age;
+	int height; 
+	
+	int j = i;
+	static int i = 10;
+	
+	
+	 // Instance variable /object variable - object creation ke time per bante hian 
+   	// static - shared , memory fix - wo class loading ke time . classes.varibale name
+	// static - instance
+	
+	
 	
 	public void speak(String content) {
 		System.out.println(name +"["+country+"]" + " - " + content);
