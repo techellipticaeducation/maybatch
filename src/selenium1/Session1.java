c2e7c557e066c2635e777b8d43f17a1ca22a80e1
Teche <techellipticaeducation@gmail.com>
2023-07-29 09:24:14 +0530

    updates

---
+++ src/selenium1/Session1.java
@@ -0,0 +1,37 @@
+package selenium1;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Session1 {
+	public static void main(String[] args) {
+		// we have to configure webdriver service
+		// webdriver.chrome.driver  - Chrome
+		// webdriver.gecko.driver  - mozilla
+		// webdriver.edge.driver - edge
+		
+		System.setProperty("webdriver.chrome.driver", 
+				"C:\\Users\\Vaibhav\\git\\maybatch\\driver\\chrome\\115\\chromedriver.exe");
+		
+		
+		
+		// xpath,css
+		
+//		for(int i = 1 ; i < 3 ;i++) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+//		driver.manage().window().setSize(new Dimension(500, 500));
+		driver.get("https://www.google.com");  // url open
+		
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getPageSource());
+		System.out.println(driver.getTitle());
+		
+		driver.close(); // current tab
+		driver.quit(); // complete session
+//		}
+		
+		
+	}
+}
